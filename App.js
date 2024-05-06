1675fa1fd8b301773b871f16c8632812f3a4e90e
Aman Kureshi <amankureshi78692@gmail.com>
2024-05-06 15:24:16 +0530

    app changes

--- App.js
+++ App.js
@@ -7,8 +7,8 @@ const demo = React.createElement("div", { id: "MainParent" },
       React.createElement("h2", { style: { color: "hotpink", border: "2px solid black", padding: "10px", borderRadius: "40px", textAlign: "center" }, className: "Parent h2" }, "this content is h2"))
   );
 
-  console.log(demo)
+  //console.log(demo)
   const demo1 = document.getElementById("root");
-  const append = ReactDOM.createRoot(demo15131);
+  const append = ReactDOM.createRoot(demo);
   append.render(demo);
   
